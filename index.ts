1d5f2c0a1ff4b0d9457212ad38e8b68fe0c1191e
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-17 16:26:50 -0400

    feat: add api

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { create, insert, search } from '@orama/orama'
 import { persistToFile, restoreFromFile } from '@orama/plugin-data-persistence/server'
 
+import Elysia from 'elysia'
+
 const imageSchema = {
     title: 'string',
     tags: 'string[]',
@@ -12,18 +14,19 @@ interface Image {
     uri: string
     owner: string
 }
+console.log("🚀 Starting server")
 
 // check if db.msp exists
+console.log("⛁  Loading db")
 let db = await create({ schema: imageSchema })
 try {
     db = await restoreFromFile('binary', 'db.msp')
 } catch (e) {
-    console.log('No db.msp file found')
+    console.log('❌ No db.msp file found; creating new db')
 }
+console.log("✅ db loaded")
 
-const searchResult = await search<typeof db, Image>(db, { term: 'example' })
-console.log(searchResult.hits.map(hit => hit.document.title))
-
+console.log("⚙️  Setting up handlers")
 process.on('SIGINT', async () => {
     await persistToFile(db, 'binary', "db.msp")
     process.exit()
@@ -33,3 +36,59 @@ process.on("SIGTERM", async () => {
     await persistToFile(db, 'binary', "db.msp")
     process.exit()
 })
+
+console.log("✅ Handlers set up")
+
+// start elysia
+console.log("🦊 Starting Elysia")
+
+const elysia = new Elysia()
+    .get('/', () => "Hello from Elysia! 🦊")
+    .get('/query', async ({ request }) => {
+        // get search query
+        const { userID, query } = await request.json()
+        const bearer = request.headers.get('Authorization');
+
+        // check if user is authenticated
+        if (bearer !== process.env.TOKEN) {
+            return { error: 'Unauthorized' }
+        }
+
+        if (!userID || !query) {
+            return { error: 'Invalid query' }
+        }
+
+        // search for images
+        const results = await search<typeof db, Image>(db, query)
+        const images = results.hits.map((hit) => hit.document)
+
+        return images.filter((image) => image.owner === userID)
+    })
+    .post('/insert', async ({ request }) => {
+        // get image data
+        const { title, tags, uri, owner } = await request.json()
+        const bearer = request.headers.get('Authorization');
+
+        // check if user is authenticated
+        if (bearer !== process.env.TOKEN) {
+            return { error: 'Unauthorized' }
+        }
+
+        // ensure that image has the same type as the interface Image
+        if (!title || !tags || !uri || !owner) {
+            return { error: 'Invalid image data' }
+        }
+
+        // insert image into db
+        try {
+            await insert(db, { title, tags, uri, owner })
+        } catch (e) {
+            return { error: 'Failed to insert image' }
+        }
+
+        return { success: true }
+    })
+    .listen(4221)
+
+console.log("✅ Elysia started at http://localhost:4221")
+
